55eb740dadd81141a2d0c5bb3a7fc0c21067c832
OksanaKriuchkova <oksana.khl@gmail.com>
2024-07-04 18:13:11 +0300

    Login Test

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,41 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void fillLoginRegistrationForm(String user, String password){
+      //  WebElement userInput = wd.findElement(By.xpath("//input[@id='username']"));
+      //  userInput.click();
+      //  userInput.clear();
+      //  userInput.sendKeys(user);
+
+        type(By.xpath("//input[@id='username']"),user);
+
+      //  WebElement passwordInput = wd.findElement(By.xpath("//input[@id='password']"));
+      //  passwordInput.click();
+      //  passwordInput.clear();
+      //  passwordInput.sendKeys(password);
+
+        type(By.xpath("//input[@id='password']"),password);
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//button[@id='loginBtn']"));
+    }
+
+    public boolean isLogged(){
+        return  isElementPresent(By.xpath("//body/app-root/app-home/div[@id='main']/div[@id='right-panel']/div[@class='top-content']/header[@class='header']/div[@class='right-top-header']/div/a[1]"));
+
+    }
+
+    public void logout(){
+        click(By.xpath("//body/app-root/app-home/div[@id='main']/div[@id='right-panel']/div[@class='top-content']/header[@class='header']/div[@class='right-top-header']/div/a[1]"));
+        click(By.xpath("//a[normalize-space()='Sign Out']"));
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,35 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
+
     @Test
-    public void test(){
+    public void loginSuccess(){
+        app.getHelperUser().fillLoginRegistrationForm("admin","REZ1ucp2drw8gdp@efj");
+        app.getHelperUser().submitLogin();
+
+       Assert.assertTrue(app.getHelperUser().isLogged());
 
     }
+
+    @Test
+    public void loginSuccess2(){
+        app.getHelperUser().fillLoginRegistrationForm("admin","REZ1ucp2drw8gdp@efj");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
+
+
 }
